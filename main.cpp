d9a20c5814b5199c8779e6d2f2c132a365cb9717
PhanDuyHung <35158975+PhanDuyHung@users.noreply.github.com>
2018-01-10 21:57:27 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,9 +9,74 @@
 struct Frame
 {
 	int id;
+	int label_traffic;
 	TrafficSign traffic;
 };
 
+void PreSave(vector<Frame>& frameOutput)
+{
+	int size = (int)frameOutput.size();
+	for (int i = 0; i < size; i++)
+	{
+		int count = 0;
+		int minindex = (i - 5 > 0) ? i - 5 : 0;
+		int maxindex = (i + 5 < size-1) ? i + 5 : size-1;
+		TrafficSign arr[11]; 
+		int freq[11];
+		for (int j = minindex, k = 0; j <= maxindex; j++)
+		{
+			arr[k++] = frameOutput[j].traffic;
+			if (frameOutput[j].traffic.getId() == frameOutput[i].traffic.getId()) count++;
+		}
+		if (count == 1) //chi xuat hien 1 lan -> replace vs bien bao khac gan nhat
+		{
+			cout << frameOutput[i].id<<"\n";
+			//tìm biển báo có tần số xuất hiện nhiều nhất
+			for (int j = 0; j<11; j++)
+			{
+				int f = 0;
+				for (int p = 1; p < 10; p++)
+				{
+					if (arr[p].getId() == arr[j].getId())
+					{
+						f++;
+					}
+				}
+				freq[j] = f;
+				if (f>5) break;
+			}
+			int maxf = 0; int labelmax = -1;
+			for (int j = 0; j<11; j++)
+			{
+				if (freq[j] > maxf && freq[j] > 5)
+				{
+					maxf = freq[j];
+					labelmax = arr[j].getId();
+				}
+			}
+			if (labelmax == -1) continue;
+			for (int j = i; j > 0 && i - j < 5; j--) //duyet tren
+			{
+				if (frameOutput[j].traffic.getId() == labelmax)
+				{
+					frameOutput[i].traffic = frameOutput[j].traffic;
+					frameOutput[i].label_traffic = frameOutput[j].label_traffic;
+					break;
+				}
+			}
+			for (int j = i; j < size && j - i < 5; j++) //duyet duoi
+			{
+				if (frameOutput[j].traffic.getId() == labelmax)
+				{
+					frameOutput[i].traffic = frameOutput[j].traffic;
+					frameOutput[i].label_traffic = frameOutput[j].label_traffic;
+					break;
+				}
+			}
+		}
+
+	}
+}
 
 void SaveFileOutput(const string& filename, const vector<Frame>& frameOutput)
 {
@@ -26,7 +91,7 @@ void SaveFileOutput(const string& filename, const vector<Frame>& frameOutput)
 		Point p0, p1;
 		if (frameOutput[i].traffic.getId() == -1) //frame hasn't traffic sign
 		{
-			file << i << "\t" << -1<<"\n";
+			file << frameOutput[i].id << "\t" << -1<<"\n";
 			continue;
 		}
 	
@@ -34,7 +99,7 @@ void SaveFileOutput(const string& filename, const vector<Frame>& frameOutput)
 		p0 = Point(frameOutput[i].traffic.getCenTer().x - length / 2, frameOutput[i].traffic.getCenTer().y - length / 2);
 		p1 = Point(frameOutput[i].traffic.getCenTer().x + length / 2, frameOutput[i].traffic.getCenTer().y + length / 2);
 		id = frameOutput[i].traffic.getId();
-		file << i << "\t" << id << "\t" << p0.x << "\t" << p0.y << "\t" << p1.x << "\t" << p1.y << "\n";
+		file << frameOutput[i].id << "\t" << id << "\t" << p0.x << "\t" << p0.y << "\t" << p1.x << "\t" << p1.y << "\n";
 	}
 	file.close();
 }
@@ -78,7 +143,7 @@ int main(int argc, wchar_t* argv[]){
 
 	vector<Frame> frameOutput;
 	
-	int idFrame = 0;
+	int idFrame = 0; 
 	while (1)
 	{
 		Mat frame;
@@ -105,6 +170,7 @@ int main(int argc, wchar_t* argv[]){
 
 			/*Push output*/
 			f.traffic = traffic;
+			f.label_traffic = img_label;
 			frameOutput.push_back(f);
 
 			PyErr_Occurred();
@@ -120,15 +186,16 @@ int main(int argc, wchar_t* argv[]){
 		MyDetecter::DrawTrafficSigns(frame, traffiSigns);
 
 		imshow("Traffic Sign", frame);
-		if (waitKey(10) >= 0) break;
+		if (waitKey(1) >= 0) break;
 
 		idFrame++;
 	}
 
 	cout << "\nWrite output file!";
+	PreSave(frameOutput);
 	SaveFileOutput("output.txt", frameOutput);
 	cout << "\nSaved!";
 
-
+	system("pause");
 	return 0;
 }
