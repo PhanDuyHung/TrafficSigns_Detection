582fc9775ff5bd229c2b756a5e9bd4f9aac85eaa
PhanDuyHung <35158975+PhanDuyHung@users.noreply.github.com>
2018-02-28 11:17:14 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,118 +6,6 @@
 /*============NOTHING IS IMPOSSIBLE===========*/
 /*1 Jan 2018*/
 
-struct Frame
-{
-	int id;
-	//int label_traffic;
-	TrafficSign traffic;
-};
-
-void PreSave(vector<Frame>& frameOutput)
-{
-	//int size = (int)frameOutput.size();
-	//for (int i = 0; i < size; i++)
-	//{
-	//	int count = 0;
-	//	int minindex = (i - 5 > 0) ? i - 5 : 0;
-	//	int maxindex = (i + 5 < size - 1) ? i + 5 : size - 1;
-	//	TrafficSign arr[11];
-	//	int freq[11];
-	//	for (int j = minindex, k = 0; j <= maxindex; j++)
-	//	{
-	//		arr[k++] = frameOutput[j].traffic;
-	//		if (frameOutput[j].traffic.getId() == frameOutput[i].traffic.getId()) count++;
-	//	}
-	//	if (count == 1) //chi xuat hien 1 lan -> replace vs bien bao khac gan nhat
-	//	{
-	//		cout << frameOutput[i].id << "\n";
-	//		//tìm biển báo có tần số xuất hiện nhiều nhất
-	//		for (int j = 0; j < 11; j++)
-	//		{
-	//			int f = 0;
-	//			for (int p = 1; p < 10; p++)
-	//			{
-	//				if (arr[p].getId() == arr[j].getId())
-	//				{
-	//					f++;
-	//				}
-	//			}
-	//			freq[j] = f;
-	//			if (f>5) break;
-	//		}
-	//		int maxf = 0; int labelmax = -1;
-	//		for (int j = 0; j<11; j++)
-	//		{
-	//			if (freq[j] > maxf && freq[j] > 5)
-	//			{
-	//				maxf = freq[j];
-	//				labelmax = arr[j].getId();
-	//			}
-	//		}
-	//		if (labelmax == -1) continue;
-	//		for (int j = i; j > 0 && i - j < 5; j--) //duyet tren
-	//		{
-	//			if (frameOutput[j].traffic.getId() == labelmax)
-	//			{
-	//				frameOutput[i].traffic = frameOutput[j].traffic;
-	//				frameOutput[i].label_traffic = frameOutput[j].label_traffic;
-	//				break;
-	//			}
-	//		}
-	//		for (int j = i; j < size && j - i < 5; j++) //duyet duoi
-	//		{
-	//			if (frameOutput[j].traffic.getId() == labelmax)
-	//			{
-	//				frameOutput[i].traffic = frameOutput[j].traffic;
-	//				frameOutput[i].label_traffic = frameOutput[j].label_traffic;
-	//				break;
-	//			}
-	//		}
-	//	}
-	//}
-
-	int size = (int)frameOutput.size();
-	int stt = frameOutput[0].id;
-	for (int i = 1; i < size; i++)
-	{
-		stt++;
-		if (frameOutput[i].id == stt)
-		{
-			continue;
-		}
-		Frame f; f.id = stt;
-		f.traffic = frameOutput[i - 1].traffic;
-		frameOutput.insert(frameOutput.begin() + i, f);
-	}
-}
-
-void SaveFileOutput(const string& filename, const vector<Frame>& frameOutput)
-{
-	fstream file;
-	file.open(filename, ios::out);
-
-	int n = (int)frameOutput.size();
-	file << n << "\n";
-	for (int i = 0; i < n; i++)
-	{
-		int id;
-		Point p0, p1;
-		if (frameOutput[i].traffic.getId() == -1) //frame hasn't traffic sign
-		{
-			file << frameOutput[i].id << "\t" << -1 << "\n";
-			continue;
-		}
-
-		//length = frameOutput[i].traffic.getLength();
-		Size size = frameOutput[i].traffic.getSize();
-		p0 = Point(frameOutput[i].traffic.getCenTer().x - size.width / 2, frameOutput[i].traffic.getCenTer().y - size.height / 2);
-		p1 = Point(frameOutput[i].traffic.getCenTer().x + size.width / 2, frameOutput[i].traffic.getCenTer().y + size.height / 2);
-		id = frameOutput[i].traffic.getId();
-		file << frameOutput[i].id << "\t" << "2" << "\t" << p0.x << "\t" << p0.y << "\t" << p1.x << "\t" << p1.y << "\n";
-	}
-	file.close();
-}
-
 int main(int argc, wchar_t* argv[]){
 
 	vector<TrafficSign> trafficSigns;
@@ -150,26 +38,19 @@ int main(int argc, wchar_t* argv[]){
 		break;
 	}
 
-	vector<Frame> frameOutput;
-	int width = (int)cap.get(CV_CAP_PROP_FRAME_WIDTH);
+	/*int width = (int)cap.get(CV_CAP_PROP_FRAME_WIDTH);
 	int height = (int)cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 	VideoWriter videoOut("NII.avi", CV_FOURCC('M', 'J', 'P', 'G'), 30, Size(width, height));
-
-	int idFrame = 0;
-
+*/
 	while (1)
 	{
 		Mat frame;
 		cap >> frame; // get a new frame from video
 		if (!frame.data) break;
 
-		Frame f; f.id = idFrame;
-		//bool isHasTraffic = false;
 		MyDetecter::DetectTrafficSigns(frame, trafficSigns);
 		for each (TrafficSign traffic in trafficSigns)
 		{
-			//isHasTraffic = true;
-
 			Mat imgTraffic = MyDetecter::CutTrafficSign(frame, traffic);
 			imwrite("imageDetected.jpg", imgTraffic);
 
@@ -182,47 +63,20 @@ int main(int argc, wchar_t* argv[]){
 			pyResult = PyObject_CallObject(pyFunc, NULL);
 			float acc = PyFloat_AsDouble(pyResult);
 
-			
-			//cout << "frame: " << idFrame << "\n";
 			if (acc > MINIMUM_ACCURACY)
 			{
 				traffic.setId(img_label);
 				MyDetecter::SetLabel(frame, traffic, acc);
 				MyDetecter::DrawTrafficSigns(frame, traffic);
-
-				/*Push output*/
-				f.traffic = traffic;
-				//f.label_traffic = img_label;
-				frameOutput.push_back(f);
+		
 			}
 		}
 		trafficSigns.clear();
 
-		/*if (!isHasTraffic)
-		{
-			f.traffic.setId(-1);
-			frameOutput.push_back(f);
-		}*/
-
-		//MyDetecter::DrawTrafficSigns(frame, trafficSigns,label_require);
-
 		imshow("Traffic Sign", frame);
 
-		videoOut.write(frame);
+		//videoOut.write(frame);
 		if (waitKey(1) >= 0) break;
-
-		idFrame++;
 	}
-
-	cout << "\nWrite output file!";
-	//PreSave(frameOutput);
-
-	videoOut.release();
-
-	//SaveFileOutput("Output.txt", frameOutput);
-	
-	cout << "\nSaved!";
-
-	//system("pause");
 	return 0;
 }
